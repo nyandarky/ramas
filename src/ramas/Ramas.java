1063620c6d85b2628f33921c712e209a54a699cc
dam <dam@203PC07.intern>
2019-01-08 09:27:02 +0100

    Versión 1.0

--- src/ramas/Ramas.java
+++ src/ramas/Ramas.java
@@ -16,7 +16,7 @@ public class Ramas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("version 1.0");
+        System.out.println("version 2.0");
     }
     
 }
