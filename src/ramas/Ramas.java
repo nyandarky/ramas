e5822bf10195536c24ddf7282e4ba5392cb65afd
dam <dam@203PC07.intern>
2019-01-08 10:04:32 +0100

    desarrollo version 2.1

--- src/ramas/Ramas.java
+++ src/ramas/Ramas.java
@@ -17,6 +17,9 @@ public class Ramas {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("version 2.0");
+        System.out.println("Estoy haciendo cambios");
+        int a=20;
+        System.out.println(a);
     }
     
 }
